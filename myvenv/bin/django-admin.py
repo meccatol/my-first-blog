81c03a960f9a21c75ad3587e8add5aaf2b71d7ff
meccatol <meccatol@gmail.com>
2018-05-01 14:01:14 +0900

    Django girls app, first commit! :D

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/meccasmacbookpro/Documents/Mecca/CreamWorld/Python/virenv/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
